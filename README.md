66d94832e8495fb8b259eeae17b66bddd67ec86e
Cristyan Henrich <cristyanhenrich16@gmail.com>
2023-12-02 13:48:15 -0300

    diretivas

---
+++ README.md
@@ -0,0 +1,1 @@
+php artisan vendor:publish --tag=livemodal-assets
--- src/ModalServiceProvider.php
+++ src/ModalServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace cristyanhenrich\livemodal;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 class ModalServiceProvider extends ServiceProvider
@@ -9,9 +10,20 @@ class ModalServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'modal');
+        
         $this->publishes([
             __DIR__.'/assets' => public_path('vendor/cristyanhenrich/livemodal'),
         ], 'livemodal-assets');
+
+        // Diretiva para estilos
+        Blade::directive('livemodalStyles', function () {
+            return '<link href="' . asset('vendor/cristyanhenrich/livemodal/css/style.css') . '" rel="stylesheet">';
+        });
+
+        // Diretiva para scripts
+        Blade::directive('livemodalScripts', function () {
+            return '<script src="' . asset('vendor/cristyanhenrich/livemodal/js/main.js') . '"></script>';
+        });
 
         Livewire::component('modal', ModalComponent::class);
     }
